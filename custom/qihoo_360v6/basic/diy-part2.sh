c54aea0ee043f5e48cc290fbc499c7f75caacb25
0118Add <60157610+0118Add@users.noreply.github.com>
2022-11-22 12:41:32 +0800

    Update diy-part2.sh

--- custom/qihoo_360v6/basic/diy-part2.sh
+++ custom/qihoo_360v6/basic/diy-part2.sh
@@ -12,5 +12,3 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-rm -rf feeds/luci/applications/luci-app-ssr-plus
-rm -rf feeds/luci/applications/luci-app-v2ray-server
