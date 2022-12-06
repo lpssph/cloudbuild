e9fb7d8bda24a4112c3115de65f0b28e9758a91d
0118Add <60157610+0118Add@users.noreply.github.com>
2022-12-06 16:47:25 +0800

    Update diy-part2.sh

--- custom/cmiot_ax18/plus/diy-part2.sh
+++ custom/cmiot_ax18/plus/diy-part2.sh
@@ -14,7 +14,7 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # Modify version
-sed -i 's/R22.11.11/V22.11.11/g' package/addition/default-settings/files/99-default-settings
+#sed -i 's/R22.11.11/V22.11.11/g' package/addition/default-settings/files/99-default-settings
 
 # 修改系统文件
 #curl -fsSL https://raw.githubusercontent.com/0118Add/OpenWrt-CI/main/ipq60xx/index.htm > ./package/addition/autocore/files/arm/index.htm
