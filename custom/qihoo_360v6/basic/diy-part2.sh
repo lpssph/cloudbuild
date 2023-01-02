b099042fa09610944fcf821d142900a1eb6ce6d8
0118Add <60157610+0118Add@users.noreply.github.com>
2023-01-02 12:44:54 +0800

    Update diy-part2.sh

--- custom/qihoo_360v6/basic/diy-part2.sh
+++ custom/qihoo_360v6/basic/diy-part2.sh
@@ -12,4 +12,4 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-curl -fsSL https://raw.githubusercontent.com/0118Add/OpenWrt-CI/main/ipq60xx/index.htm > ./package/addition/autocore/files/arm/index.htm
+#curl -fsSL https://raw.githubusercontent.com/0118Add/OpenWrt-CI/main/ipq60xx/index.htm > ./package/addition/autocore/files/arm/index.htm
