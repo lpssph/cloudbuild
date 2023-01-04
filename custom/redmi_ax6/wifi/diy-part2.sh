928a782a030c9e98b1667373f19b8b69dd02f32c
0118Add <60157610+0118Add@users.noreply.github.com>
2023-01-04 22:04:33 +0800

    Update diy-part2.sh

--- custom/redmi_ax6/wifi/diy-part2.sh
+++ custom/redmi_ax6/wifi/diy-part2.sh
@@ -12,3 +12,4 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i "s/DISTRIB_REVISION='*.*'/DISTRIB_REVISION='R23.1.1'/g" package/lean/default-settings/files/zzz-default-settings
