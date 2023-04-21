3b1f2dba2f85eb6198255880828cba86a5f76e85
0118Add <60157610+0118Add@users.noreply.github.com>
2023-04-21 22:37:24 +0800

    Update diy-part1.sh

--- custom/qihoo_360v6/basic/diy-part1.sh
+++ custom/qihoo_360v6/basic/diy-part1.sh
@@ -14,5 +14,5 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld.git^b292dcafd5706c9f0545dc1848b34d3b7c8be49d' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
