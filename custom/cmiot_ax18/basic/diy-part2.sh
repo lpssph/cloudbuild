7b47885ce0ffc91e0b8f6b5f943b7a397d3be2d7
0118Add <60157610+0118Add@users.noreply.github.com>
2023-06-29 16:54:31 +0800

    Update diy-part2.sh

--- custom/cmiot_ax18/basic/diy-part2.sh
+++ custom/cmiot_ax18/basic/diy-part2.sh
@@ -12,5 +12,5 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed -i 's/os.date()/os.date("%Y年%m月%d日") .. " " .. translate(os.date("%A")) .. " " .. os.date("%X")/g' package/extra/autocore/files/generic/index.htm
+#sed -i 's/os.date()/os.date("%Y年%m月%d日") .. " " .. translate(os.date("%A")) .. " " .. os.date("%X")/g' package/extra/autocore/files/generic/index.htm
 #sed -i 's/os.date()/os.date("%Y年%m月%d日") .. " " .. translate(os.date("%A")) .. " " .. os.date("%X")/g' package/emortal/autocore/files/generic/index.htm
