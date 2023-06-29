8ccdbae022658c5fada3ece07e4efab58a54ed97
0118Add <60157610+0118Add@users.noreply.github.com>
2023-06-29 23:01:18 +0800

    Update diy-part2.sh

--- custom/cmiot_ax18/plus/diy-part2.sh
+++ custom/cmiot_ax18/plus/diy-part2.sh
@@ -26,7 +26,7 @@ git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
 #svn co https://github.com/vernesong/OpenClash/branches/dev/luci-app-openclash package/luci-app-openclash
 svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
 git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
-git clone https://github.com/sirpdboy/luci-app-ddns-go package/luci-app-ddns-go
+#git clone https://github.com/sirpdboy/luci-app-ddns-go package/luci-app-ddns-go
 git clone https://github.com/gngpp/luci-theme-design package/luci-theme-design
 git clone https://github.com/gngpp/luci-app-design-config package/luci-app-design-config
 
