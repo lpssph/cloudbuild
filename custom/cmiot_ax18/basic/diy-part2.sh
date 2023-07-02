5767073d0dc1f5df8f82436e428860260a93b5ca
0118Add <60157610+0118Add@users.noreply.github.com>
2023-07-02 15:43:19 +0800

    Update diy-part2.sh

--- custom/cmiot_ax18/basic/diy-part2.sh
+++ custom/cmiot_ax18/basic/diy-part2.sh
@@ -66,3 +66,6 @@ MT_WIFI(){
 MTK_HNAT
 echo '-------------------------------------------------------------------------------------------------'
 MT_WIFI
+
+./REVISECONFIG.sh tc-tiny kmod-sched kmod-sched-cake kmod-sched-connmark kmod-sched-ipset
+echo '-----------------------------------------------'
