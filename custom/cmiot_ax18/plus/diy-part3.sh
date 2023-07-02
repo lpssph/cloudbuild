22bfe9b29c63935755cba43f384a95f0e40d5242
0118Add <60157610+0118Add@users.noreply.github.com>
2023-07-02 16:25:41 +0800

    Update diy-part3.sh

--- custom/cmiot_ax18/plus/diy-part3.sh
+++ custom/cmiot_ax18/plus/diy-part3.sh
@@ -6,18 +6,101 @@
 # See /LICENSE for more information.
 #
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+VERSION="V3.4"
 
-# Add a feed source
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#overwall测试版
-#sed -i '$a src-git overwall https://github.com/wangzan200808/overwall' feeds.conf.default
-#sed -i '$a src-git wz https://github.com/wangzan200808/bypackages' feeds.conf.default
-#正常编译bypass
-#sed -i '$a src-git wz https://github.com/wangzan200808/bypackage' feeds.conf.default
-#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+cat > version.patch  <<EOF
+--- a/package/base-files/files/etc/banner
++++ b/package/base-files/files/etc/banner
+@@ -4,5 +4,5 @@
+  |_______||   __|_____|__|__||________||__|  |____|
+           |__| W I R E L E S S   F R E E D O M
+  -----------------------------------------------------
+- %D %V, %C
++ %D $VERSION By Zan, %C
+  -----------------------------------------------------
+
+--- a/package/base-files/files/etc/openwrt_release
++++ b/package/base-files/files/etc/openwrt_release
+@@ -1,7 +1,7 @@
+ DISTRIB_ID='%D'
+-DISTRIB_RELEASE='%V'
++DISTRIB_RELEASE='$VERSION By Zan'
+ DISTRIB_REVISION='%R'
+ DISTRIB_TARGET='%S'
+ DISTRIB_ARCH='%A'
+-DISTRIB_DESCRIPTION='%D %V %C'
++DISTRIB_DESCRIPTION='%D $VERSION By Zan %C'
+ DISTRIB_TAINTS='%t'
+
+--- a/package/base-files/files/usr/lib/os-release
++++ b/package/base-files/files/usr/lib/os-release
+@@ -1,8 +1,8 @@
+ NAME="%D"
+-VERSION="%V"
++VERSION="$VERSION By Zan"
+ ID="%d"
+ ID_LIKE="lede openwrt"
+-PRETTY_NAME="%D %V"
++PRETTY_NAME="%D $VERSION By Zan"
+ VERSION_ID="%v"
+ HOME_URL="%u"
+ BUG_URL="%b"
+@@ -15,4 +15,4 @@
+ OPENWRT_DEVICE_MANUFACTURER_URL="%m"
+ OPENWRT_DEVICE_PRODUCT="%P"
+ OPENWRT_DEVICE_REVISION="%h"
+-OPENWRT_RELEASE="%D %V %C"
++OPENWRT_RELEASE="%D $VERSION By Zan %C"
+EOF
+
+patch -p1 -E < version.patch && rm -f version.patch
+for i in $(find -maxdepth 1 -name 'Patch-*.patch' | sed 's#.*/##');do
+	patch -p1 -E < $i
+done
+rm -f Patch-*.patch
+
+sed -i 's/os.date()/os.date("%Y年%m月%d日") .. " " .. translate(os.date("%A")) .. " " .. os.date("%X")/g' package/emortal/autocore/files/generic/index.htm
+
+# 更改固件版本信息
+sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='OpenWrt 18.06'/g" package/base-files/files/etc/openwrt_release
+sed -i "s/DISTRIB_REVISION='*.*'/DISTRIB_REVISION=''/g" package/base-files/files/etc/openwrt_release
+
+# 添加删除软件
+rm -rf feeds/luci/applications/luci-app-vssr
+rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-ssr-plus
+#rm -rf feeds/luci/applications/luci-app-openclash
+svn co https://github.com/kiddin9/openwrt-packages/trunk/lua-maxminddb package/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
+#svn co https://github.com/vernesong/OpenClash/branches/dev/luci-app-openclash package/luci-app-openclash
+svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
+git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
+git clone https://github.com/sirpdboy/luci-app-ddns-go package/luci-app-ddns-go
+git clone https://github.com/gngpp/luci-theme-design package/luci-theme-design
+git clone https://github.com/gngpp/luci-app-design-config package/luci-app-design-config
+
+# name: 替换默认主题 luci-theme-argon
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+
+# 默认ip 192.168.1.1
+#sed -i 's/192.168.[0-9]\{1,3\}.1/192.168.1.1/g' package/base-files/files/bin/config_generate
+
+# 修改时区 UTF-8
+sed -i 's/UTC/CST-8/g'  package/base-files/files/bin/config_generate
+
+# 修改主机名 OP
+sed -i 's/ImmortalWrt/OpenWrt/g'  package/base-files/files/bin/config_generate
+
+# 时区
+sed -i 's/time1.apple.com/time1.cloud.tencent.com/g'  package/base-files/files/bin/config_generate
+sed -i 's/time1.google.com/ntp.aliyun.com/g'  package/base-files/files/bin/config_generate
+sed -i 's/time.cloudflare.com/cn.ntp.org.cn/g'  package/base-files/files/bin/config_generate
+sed -i 's/pool.ntp.org/cn.pool.ntp.org/g'  package/base-files/files/bin/config_generate
+
+# 替换源 
+sed -i 's,mirrors.vsean.net/openwrt,mirrors.pku.edu.cn/immortalwrt,g'  package/emortal/default-settings/files/99-default-settings-chinese
