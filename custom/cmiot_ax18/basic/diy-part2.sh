495580c82d0bf7353f710ee8968c806894ae9e08
0118Add <60157610+0118Add@users.noreply.github.com>
2023-07-02 11:59:41 +0800

    Update diy-part2.sh

--- custom/cmiot_ax18/basic/diy-part2.sh
+++ custom/cmiot_ax18/basic/diy-part2.sh
@@ -12,8 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-#sed -i 's/os.date()/os.date("%Y年%m月%d日") .. " " .. translate(os.date("%A")) .. " " .. os.date("%X")/g' package/extra/autocore/files/generic/index.htm
-#sed -i 's/os.date()/os.date("%Y年%m月%d日") .. " " .. translate(os.date("%A")) .. " " .. os.date("%X")/g' package/emortal/autocore/files/generic/index.htm
 
 # 更改固件版本信息
 sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='OpenWrt 21.02'/g" package/base-files/files/etc/openwrt_release
