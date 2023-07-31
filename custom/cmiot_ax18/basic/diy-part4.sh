e1aebf90ecf69235567b8ccc7ffb3440042770ea
0118Add <60157610+0118Add@users.noreply.github.com>
2023-07-31 20:57:17 +0800

    Update diy-part4.sh

--- custom/cmiot_ax18/basic/diy-part4.sh
+++ custom/cmiot_ax18/basic/diy-part4.sh
@@ -12,57 +12,26 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
-# 更改固件版本信息
-sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='OpenWrt 21.02'/g" package/base-files/files/etc/openwrt_release
-sed -i "s/DISTRIB_REVISION='*.*'/DISTRIB_REVISION=''/g" package/base-files/files/etc/openwrt_release
+sed -i 's/os.date()/os.date("%Y年%m月%d日") .. " " .. translate(os.date("%A")) .. " " .. os.date("%X")/g' package/extra/autocore/files/generic/index.htm
 
 # 添加删除软件
 rm -rf feeds/luci/applications/luci-app-vssr
 rm -rf feeds/luci/applications/luci-app-passwall
-rm -rf feeds/luci/applications/luci-app-ssr-plus
-#rm -rf feeds/luci/applications/luci-app-openclash
+#rm -rf feeds/luci/applications/luci-app-ssr-plus
+rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-unblockneteasemusic
 svn co https://github.com/kiddin9/openwrt-packages/trunk/lua-maxminddb package/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
-svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
-#git clone https://github.com/sirpdboy/luci-app-ddns-go package/luci-app-ddns-go
-git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
 #svn co https://github.com/vernesong/OpenClash/branches/dev/luci-app-openclash package/luci-app-openclash
+#svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
+svn co https://github.com/fw876/helloworld/trunk/tuic-client package/tuic-client
+git clone https://github.com/sirpdboy/luci-app-ddns-go package/luci-app-ddns-go
+git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
+git clone https://github.com/0118Add/luci-app-unblockneteasemusic package/luci-app-unblockneteasemusic
+git clone https://github.com/gngpp/luci-app-design-config package/luci-app-design-config
+svn co https://github.com/vernesong/OpenClash/branches/dev/luci-app-openclash package/luci-app-openclash
 
-# 替换默认主题 luci-theme-argon
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
-
-# 默认ip 192.168.1.1
-#sed -i 's/192.168.[0-9]\{1,3\}.1/192.168.1.1/g' package/base-files/files/bin/config_generate
-
-# 修改时区 UTF-8
-sed -i 's/UTC/CST-8/g'  package/base-files/files/bin/config_generate
-
-# 修改主机名 OP
-sed -i 's/ImmortalWrt/OpenWrt/g'  package/base-files/files/bin/config_generate
-
-# 时区
-sed -i 's/time1.apple.com/time1.cloud.tencent.com/g'  package/base-files/files/bin/config_generate
-sed -i 's/time1.google.com/ntp.aliyun.com/g'  package/base-files/files/bin/config_generate
-sed -i 's/time.cloudflare.com/cn.ntp.org.cn/g'  package/base-files/files/bin/config_generate
-sed -i 's/pool.ntp.org/cn.pool.ntp.org/g'  package/base-files/files/bin/config_generate
-
-# 替换源 
-sed -i 's,mirrors.vsean.net/openwrt,mirrors.pku.edu.cn/immortalwrt,g'  package/emortal/default-settings/files/99-default-settings-chinese
-
-# 
-MTK_HNAT(){
-  rm -rf target/linux/mediatek/files-5.4/drivers/net/ethernet/mediatek/mtk_hnat
-  svn co https://github.com/padavanonly/immortalwrtARM/branches/mt7981/target/linux/mediatek/files-5.4/drivers/net/ethernet/mediatek/mtk_hnat
-  mv mtk_hnat target/linux/mediatek/files-5.4/drivers/net/ethernet/mediatek/mtk_hnat
-}
-
-MT_WIFI(){
-  rm -rf package/mtk/drivers/mt_wifi
-  svn co https://github.com/padavanonly/immortalwrtARM/branches/mt7981/package/mtk/drivers/mt_wifi
-  mv mt_wifi package/mtk/drivers/mt_wifi
-}
-
-MTK_HNAT
-echo '-------------------------------------------------------------------------------------------------'
-MT_WIFI
+# 修改插件名字
+#sed -i 's/ShadowSocksR Plus+/SSR Plus+/g' feeds/luci/applications/luci-app-ssr-plus/luasrc/controller/shadowsocksr.lua
+sed -i 's/解除网易云音乐播放限制/音乐解锁/g' package/luci-app-unblockneteasemusic/luasrc/controller/unblockneteasemusic.lua
+sed -i 's/Frp 内网穿透/内网穿透/g' feeds/luci/applications/luci-app-frpc/po/zh-cn/frp.po
