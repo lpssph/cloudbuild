e57e1f5e04e120ea309116a8c16c6420cd04e73a
0118Add <60157610+0118Add@users.noreply.github.com>
2024-01-07 20:08:15 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -31,7 +31,7 @@ echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
 echo "CONFIG_MTK_MEMORY_SHRINK=$([[ $WRT_SHRINK == "true" ]] && echo "y" || echo "n")" >> ./.config
 echo "CONFIG_MTK_MEMORY_SHRINK_AGGRESS=$([[ $WRT_SHRINK == "true" ]] && echo "y" || echo "n")" >> ./.config
 
-rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata}
+rm -rf feeds/packages/net/{hysteria,xray-core,v2ray-core,v2ray-geodata}
 rm -rf package/emortal/automount
 rm -rf feeds/luci/applications/luci-app-ssr-plus
 rm -rf feeds/luci/applications/luci-app-vssr
