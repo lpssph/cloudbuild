7765c7dbd7e276318140911b0c2374f56f56cd3c
0118Add <60157610+0118Add@users.noreply.github.com>
2024-05-17 13:40:56 +0800

    Update diy-part4.sh

--- custom/cmiot_ax18/basic/diy-part4.sh
+++ custom/cmiot_ax18/basic/diy-part4.sh
@@ -68,6 +68,6 @@ git clone https://github.com/gngpp/luci-app-design-config package/luci-app-desig
 git clone -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush package/luci-app-wechatpush
 
 # 修改插件名字
-sed -i 's/ShadowSocksR Plus+/SSR Plus+/g' package/helloworld/luci-app-ssr-plus/luasrc/controller/shadowsocksr.lua
+sed -i 's/ShadowSocksR Plus+/SSR Plus+/g' package/openwrt_helloworld/luci-app-ssr-plus/luasrc/controller/shadowsocksr.lua
 sed -i 's/解除网易云音乐播放限制/音乐解锁/g' package/luci-app-unblockneteasemusic/luasrc/controller/unblockneteasemusic.lua
 #sed -i 's/Frp 内网穿透/内网穿透/g' feeds/luci/applications/luci-app-frpc/po/zh-cn/frp.po
