d1789f53cff1b14c889d35be27a491bdcc4e61d1
0118Add <60157610+0118Add@users.noreply.github.com>
2024-05-23 17:11:43 +0800

    Update common.sh

--- common/common.sh
+++ common/common.sh
@@ -26,7 +26,7 @@ Compte=$(date +%Y年%m月%d号%H时%M分)
 
 function settings_variable() {
 cd ${GITHUB_WORKSPACE}
-bash <(curl -fsSL https://raw.githubusercontent.com/shidahuilang/common/main/custom/first.sh)
+bash <(curl -fsSL https://raw.githubusercontent.com/0118Add/CloudBuild/main/common/custom/first.sh)
 }
 
 function Diy_variable() {
