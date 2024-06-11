8e99a94463a060df0f46280672ede1e185473351
0118Add <60157610+0118Add@users.noreply.github.com>
2024-06-11 20:24:26 +0800

    Update diy-part2.sh

--- custom/IPQ60XX/diy-part2.sh
+++ custom/IPQ60XX/diy-part2.sh
@@ -71,6 +71,3 @@ find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/luci
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/lang\/golang\/golang-package.mk/$(TOPDIR)\/feeds\/packages\/lang\/golang\/golang-package.mk/g' {}
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=@GHREPO/PKG_SOURCE_URL:=https:\/\/github.com/g' {}
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=@GHCODELOAD/PKG_SOURCE_URL:=https:\/\/codeload.github.com/g' {}
-
-./scripts/feeds update -a
-./scripts/feeds install -a
