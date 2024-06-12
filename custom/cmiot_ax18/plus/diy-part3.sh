8978a15977e9ea7cb7f1c1cf9c54a2e2eb3b72d2
0118Add <60157610+0118Add@users.noreply.github.com>
2024-06-12 13:38:26 +0800

    Update diy-part3.sh

--- custom/cmiot_ax18/plus/diy-part3.sh
+++ custom/cmiot_ax18/plus/diy-part3.sh
@@ -10,7 +10,7 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-echo "开始 DIY2 配置……"
+echo "开始配置……"
 echo "========================="
 
 function merge_package(){
@@ -44,6 +44,7 @@ sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 #sed -i "s/DISTRIB_REVISION='*.*'/DISTRIB_REVISION=''/g" package/base-files/files/etc/openwrt_release
 
 # 添加删除软件
+rm -rf feeds/packages/net/{chinadns-ng,hysteria,xray-core,v2ray-core,v2ray-geodata,sing-box,shadowsocks-rust,shadowsocksr-libev}
 rm -rf feeds/luci/applications/luci-app-smartdns
 rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/luci/applications/luci-app-ssr-plus
@@ -52,17 +53,17 @@ merge_package https://github.com/kiddin9/openwrt-packages openwrt-packages/lua-m
 merge_package https://github.com/kiddin9/openwrt-packages openwrt-packages/luci-app-smartdns
 #git clone https://github.com/0118Add/luci-app-vssr package/luci-app-vssr
 git clone --depth=1 -b luci-smartdns-dev https://github.com/xiaorouji/openwrt-passwall package/passwall
-merge_package https://github.com/xiaorouji/openwrt-passwall-packages openwrt-passwall-packages/sing-box
-merge_package https://github.com/xiaorouji/openwrt-passwall-packages openwrt-passwall-packages/tuic-client
-merge_package https://github.com/xiaorouji/openwrt-passwall-packages openwrt-passwall-packages/chinadns-ng
-merge_package https://github.com/xiaorouji/openwrt-passwall-packages openwrt-passwall-packages/hysteria
-merge_package https://github.com/xiaorouji/openwrt-passwall-packages openwrt-passwall-packages/shadowsocksr-libev
-merge_package https://github.com/xiaorouji/openwrt-passwall-packages openwrt-passwall-packages/v2ray-core
-merge_package https://github.com/xiaorouji/openwrt-passwall-packages openwrt-passwall-packages/v2ray-geodata
-merge_package https://github.com/0118Add/helloworld helloworld/shadowsocks-rust
-merge_package https://github.com/0118Add/helloworld helloworld/xray-core
-merge_package https://github.com/0118Add/helloworld helloworld/shadow-tls
-merge_package https://github.com/0118Add/helloworld helloworld/luci-app-ssr-plus
+merge_package https://github.com/sbwml/openwrt_helloworld openwrt_helloworld/sing-box
+merge_package https://github.com/sbwml/openwrt_helloworld openwrt_helloworld/tuic-client
+merge_package https://github.com/sbwml/openwrt_helloworld openwrt_helloworld/chinadns-ng
+merge_package https://github.com/sbwml/openwrt_helloworld openwrt_helloworld/hysteria
+merge_package https://github.com/sbwml/openwrt_helloworld openwrt_helloworld/shadowsocksr-libev
+merge_package https://github.com/sbwml/openwrt_helloworld openwrt_helloworld/v2ray-core
+merge_package https://github.com/sbwml/openwrt_helloworld openwrt_helloworld/v2ray-geodata
+merge_package https://github.com/sbwml/openwrt_helloworld openwrt_helloworld/shadowsocks-rust
+merge_package https://github.com/sbwml/openwrt_helloworld openwrt_helloworld/xray-core
+merge_package https://github.com/sbwml/openwrt_helloworld openwrt_helloworld/shadow-tls
+merge_package https://github.com/sbwml/openwrt_helloworld openwrt_helloworld/luci-app-ssr-plus
 merge_package https://github.com/kiddin9/openwrt-packages openwrt-packages/luci-app-bypass
 git clone https://github.com/sirpdboy/luci-app-ddns-go package/luci-app-ddns-go
 git clone https://github.com/gngpp/luci-theme-design package/luci-theme-design
