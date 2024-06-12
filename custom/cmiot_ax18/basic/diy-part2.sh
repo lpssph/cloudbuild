59d7988ede266b338972dbd268af08991beab69e
0118Add <60157610+0118Add@users.noreply.github.com>
2024-06-12 14:07:30 +0800

    Update diy-part2.sh

--- custom/cmiot_ax18/basic/diy-part2.sh
+++ custom/cmiot_ax18/basic/diy-part2.sh
@@ -42,6 +42,7 @@ sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 #sed -i "s/DISTRIB_REVISION='*.*'/DISTRIB_REVISION=''/g" package/base-files/files/etc/openwrt_release
 
 # 添加删除软件
+rm -rf feeds/luci/applications/luci-app-ddns-go
 rm -rf feeds/luci/applications/luci-app-vssr
 rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/luci/applications/luci-app-ssr-plus
